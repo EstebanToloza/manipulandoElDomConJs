ab8a189d31d5f6d4de2eaf27133d0b122ab708db
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-09-11 12:56:41 -0300

    Iniciando proyectos. Editando README

---
+++ 0_fundamentos/hola_mundo_js/compartamiento.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+
+    let parrafo = document.querySelector('p')
+
+    parrafo.onclick = function(){
+        alert("Clickeaste el párrafo!")
+    }
+
+    document.querySelector('h1').onclick = function(){
+        alert("Clickeaste el titulo!")
+    }
+
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# manipulandoElDomConJs
+MANIPULANDO EL DOM CON JAVASCRIPT
+<br>
+Ejercicios y proyectos que tienen como objetivo mostrar como es posible brindarle interactividad al DOM mediante el uso de JavaScript.
