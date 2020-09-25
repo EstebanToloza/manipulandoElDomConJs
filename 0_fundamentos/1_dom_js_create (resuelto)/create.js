7425fefb43f4669926616122e52f39958210008e
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-09-25 17:30:47 -0300

    Creando elementos en el DOM con JS

--- 0_fundamentos/1_dom_js_create (resuelto)/create.js
+++ 0_fundamentos/1_dom_js_create (resuelto)/create.js
@@ -4,7 +4,22 @@ window.onload = function(){
     //METODO #1: Creación del objeto
     document.querySelector('#btn-crear-elemento-1').onclick = function(){
         
-        //TO DO
+        let tbody = document.querySelector('tbody')
+        let q_filas = tbody.childElementCount
+
+        let nueva_fila = document.createElement('tr')
+
+        let nueva_celda = document.createElement('td')
+        nueva_celda.innerHTML = q_filas + 1
+        nueva_fila.appendChild(nueva_celda)
+
+        for (let i = 0; i < 3; i++) {
+            let nueva_celda = document.createElement('td')
+            nueva_celda.innerHTML = 'Valor ' + (i + 1)
+            nueva_fila.appendChild(nueva_celda)
+        }
+
+        tbody.appendChild(nueva_fila)
 
     }
 
@@ -12,7 +27,12 @@ window.onload = function(){
     //METODO #2: Modificación del innerHTML
     document.querySelector('#btn-crear-elemento-2').onclick = function(){
 
-        //TO DO
+        let tbody = document.querySelector('tbody')
+        let q_filas = tbody.childElementCount
+
+        let nueva_fila = "<tr><td>" + (q_filas + 1) + "</td><td>Valor 1</td><td>Valor 2</td><td>Valor 3</td></tr>"
+
+        tbody.innerHTML = tbody.innerHTML + nueva_fila
 
     }
 
@@ -20,8 +40,14 @@ window.onload = function(){
     //METODO #3: Clonación de objeto
     document.querySelector('#btn-crear-elemento-3').onclick = function(){
         
-        //TO DO
+        let tbody = document.querySelector('tbody')
+        let q_filas = tbody.childElementCount
 
+        let fila_modelo = tbody.querySelector('tr')
+        let nueva_fila = fila_modelo.cloneNode(true)
+
+        nueva_fila.children[0].innerHTML = q_filas + 1
+        tbody.appendChild(nueva_fila)
     }
 
     //4) Creación y posicionamiento específico de un nuevo elemento en el DOM
